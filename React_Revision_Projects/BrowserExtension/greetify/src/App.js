3d12c6cb0d21388d418d0b6421e31f921a3f8ed7
Chandan8585 <chandanwingshr@gmail.com>
2023-11-17 07:35:05 +0530

    react-revision

---
+++ React_Revision_Projects/BrowserExtension/greetify/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import { images } from './assets/Image';
+function App() {
+  const randomIndex = Math.floor(Math.random()*images.length);
+  return (
+    <div className="App">
+   
+   
+   
+    </div>
+  );
+}
+
+export default App;
