16b40a4a8077ed00c36eca986182427489f618f1
Chandan8585 <chandanwingshr@gmail.com>
2024-03-03 16:32:41 +0530

    created db schema

--- employee_mgmt/backend/index.js
+++ employee_mgmt/backend/index.js
@@ -1,1 +1,30 @@
-console.log('checking')
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const authRouter = require("./routes/auth.router");
+const connectDB = require("./config/dbConfig");
+const app = express();
+
+const Port = 8080;
+
+app.use(cors());
+app.use(express.json());
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+
+app.use("/api/auth", authRouter);
+
+mongoose.connection.once("open", () => {
+  console.log("connected to DB");
+  app.listen(process.env.PORT || Port, () => {
+    console.log("Server is running");
+  });
+});
