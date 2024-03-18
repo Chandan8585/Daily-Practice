7649d416791f56cefc904997352a47071d30badf
Chandan8585 <chandanwingshr@gmail.com>
2024-03-18 22:36:09 +0530

    setup react ui

---
+++ wtiCabs/Frontend/client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ClientsPanel from './ClientsPanel';
+
+function App() {
+  return (
+    <div className="App">
+   <ClientsPanel/>
+    </div>
+  );
+}
+
+export default App;
